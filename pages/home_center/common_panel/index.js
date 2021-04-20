945d6fceecbb5db9d82e1b0616ab38c5de7773af
pengpengzhaoEddie <972012113@qq.com>
2021-04-20 16:28:00 +0800

    first commit add Electricity statistics

---
+++ pages/home_center/common_panel/index.js
@@ -0,0 +1,227 @@
+// miniprogram/pages/home_center/common_panel/index.js.js
+import { getDevFunctions, getDeviceDetails, deviceControl } from '../../../utils/api/device-api'
+import wxMqtt from '../../../utils/mqtt/wxMqtt'
+import * as echarts from '../../../ec-canvas/echarts';
+const app = getApp()
+function initChart(canvas, width, height) {
+  const chart = echarts.init(canvas, null, {
+    width: 410,
+    height: 250
+  });
+  canvas.setChart(chart);
+
+  var option = {
+    title: {
+      text: '智能单插用电量',
+      left: 'center'
+    },
+    color: ["#37A2DA"],
+    tooltip: {
+      show: true,
+      trigger: 'axis'
+    },
+    xAxis: {
+      type: 'category',
+      boundaryGap: false,
+      data: ['一', '二', '三', '四', '五', '六', '日'],
+      // x轴的字体样式
+      axisLabel: {
+        show: true,
+        textStyle: {
+          color: '#000',
+          fontSize: '14',
+        }
+      },
+      // 控制网格线是否显示
+      splitLine: {
+        show: true,
+        //  改变轴线颜色
+        lineStyle: {
+          // 使用深浅的间隔色
+          color: ['#aaaaaa']
+        }
+      },
+      // x轴的颜色和宽度
+      axisLine: {
+        lineStyle: {
+          color: '#000',
+          width: 1,   //这里是坐标轴的宽度,可以去掉
+        }
+      }
+    },
+    yAxis: {
+      x: 'center',
+      type: 'value',
+      splitLine: {
+        lineStyle: {
+          type: 'dashed'
+        }
+      },
+    },
+    series: [{
+      name: 'A',
+      type: 'line',
+      smooth: true,
+      data: [15, 2, 30, 16, 10, 17, 15]
+    }]
+  };
+
+  chart.setOption(option);
+  return chart;
+}
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    device_name: '',
+    titleItem: {
+      name: '',
+      value: '',
+    },
+    roDpList: {}, //只上报功能点
+    rwDpList: {}, //可上报可下发功能点
+    isRoDpListShow: false,
+    isRwDpListShow: false,
+    forest: '../../../image/forest@2x.gif',
+    open_image: '../../../image/Open.png',
+    close_image: '../../../image/Close.png',
+    ec: {
+      onInit: initChart
+    }
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    const { device_id } = options
+    this.setData({ device_id })
+
+    // mqtt消息监听
+    wxMqtt.on('message', (topic, newVal) => {
+      const { status } = newVal
+      console.log(newVal)
+      this.updateStatus(status)
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: async function () {
+    const { device_id } = this.data
+    const [{ name, status, icon }, { functions = [] }] = await Promise.all([
+      getDeviceDetails(device_id),
+      getDevFunctions(device_id),
+    ]);
+
+    const { roDpList, rwDpList } = this.reducerDpList(status, functions)
+
+    // 获取头部展示功能点信息
+    let titleItem = {
+      name: '',
+      value: '',
+    };
+    if (Object.keys(roDpList).length > 0) {
+      let keys = Object.keys(roDpList)[0];
+      titleItem = roDpList[keys];
+    } else {
+      let keys = Object.keys(rwDpList)[0];
+      titleItem = rwDpList[keys];
+    }
+
+    const roDpListLength = Object.keys(roDpList).length
+    const isRoDpListShow = Object.keys(roDpList).length > 0
+    const isRwDpListShow = Object.keys(rwDpList).length > 0
+
+    this.setData({ titleItem, roDpList, rwDpList, device_name: name, isRoDpListShow, isRwDpListShow, roDpListLength, icon })
+  },
+
+  // 测试微信小程序中使用echarts
+  onShareAppMessage: function (res) {
+    return {
+      title: '测试ECharts！',
+      path: '/pages/index/index',
+      success: function () { },
+      fail: function () { }
+    }
+  },
+
+  // 分离只上报功能点，可上报可下发功能点
+  reducerDpList: function (status, functions) {
+    // 处理功能点和状态的数据
+    let roDpList = {};
+    let rwDpList = {};
+    if (status && status.length) {
+      status.map((item) => {
+        const { code, value } = item;
+        let isExit = functions.find(element => element.code == code);
+        if (isExit) {
+          let rightvalue = value
+          // 兼容初始拿到的布尔类型的值为字符串类型
+          if (isExit.type === 'Boolean') {
+            rightvalue = value == 'true'
+          }
+
+          rwDpList[code] = {
+            code,
+            value: rightvalue,
+            type: isExit.type,
+            values: isExit.values,
+            name: isExit.name,
+          };
+        } else {
+          roDpList[code] = {
+            code,
+            value,
+            name: code,
+          };
+        }
+      });
+    }
+    return { roDpList, rwDpList }
+  },
+
+  sendDp: async function (e) {
+    const { dpCode, value } = e.detail
+    const { device_id } = this.data
+
+    const { success } = await deviceControl(device_id, dpCode, value)
+  },
+
+  updateStatus: function (newStatus) {
+    let { roDpList, rwDpList, titleItem } = this.data
+
+    newStatus.forEach(item => {
+      const { code, value } = item
+
+      if (typeof roDpList[code] !== 'undefined') {
+        roDpList[code]['value'] = value;
+      } else if (rwDpList[code]) {
+        rwDpList[code]['value'] = value;
+      }
+    })
+
+    // 更新titleItem
+    if (Object.keys(roDpList).length > 0) {
+      let keys = Object.keys(roDpList)[0];
+      titleItem = roDpList[keys];
+    } else {
+      let keys = Object.keys(rwDpList)[0];
+      titleItem = rwDpList[keys];
+    }
+ 
+    this.setData({ titleItem, roDpList: { ...roDpList }, rwDpList: { ...rwDpList } })
+  },
+
+  jumpTodeviceEditPage: function(){
+    console.log('jumpTodeviceEditPage')
+    const { icon, device_id, device_name } = this.data
+    wx.navigateTo({
+      url: `/pages/home_center/device_manage/index?device_id=${device_id}&device_name=${device_name}&device_icon=${icon}&open_image=${open_image}&close_image=${close_image}`,
+    })
+  }
+})
